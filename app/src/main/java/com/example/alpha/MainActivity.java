af49cfa29f1733edc2b3ca7b7b32069e3f73c577
Harshit <meharshitkapoor7@gmail.com>
2020-01-30 02:39:39 +0530

    a

--- app/src/main/java/com/example/alpha/MainActivity.java
+++ app/src/main/java/com/example/alpha/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("abey saale");
     }
+
 }
