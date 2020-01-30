413536bec45a950d3251eb1f3e50fe7b89e5fcea
arushacharyamnnit <arushacharya459@gmail.com>
2020-01-30 20:47:37 +0530

    Merge branch 'Developer' of C:\Users\User\AndroidStudioProjects\Alpha with conflicts.

--- app/src/main/java/com/example/alpha/MainActivity.java
+++ app/src/main/java/com/example/alpha/MainActivity.java
@@ -1,17 +1,61 @@
 package com.example.alpha;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+
+import android.net.Uri;
+
+//>>>>>>> Stashed changes
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 
+import java.net.URL;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-    @Override
+    private ArrayList<Integer> items;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        layoutManager = new GridLayoutManager(get);
+        recyclerView.setLayoutManager(layoutManager);
+
+        data_fetcher("https://cricapi.com/api/cricket/?apikey=Bd8wF5XUVGRFmScoOnpJ5aEh93d2");
+        }
+
+    private void data_fetcher(String url)
+    {
+class data_fetch extends AsyncTask<Void,Void,String>
+{
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+
+        return null;
     }
     public void data_fetcher(URL url)
     {
@@ -19,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
             protected String doInBackground(Void... urls) {
                 try{
-                    URL url=new URL()
+                    URL url=new URL();
                 }
                 return null;
             }
@@ -27,4 +71,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
+    data_fetch data_fetch=new data_fetch();
+    data_fetch.execute();
+    }
 }
