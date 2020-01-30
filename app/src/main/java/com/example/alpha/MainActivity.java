b67dff6e3694d8dd2d868a40098f106cc57072db
Sakar <singhalsakar@gmail.com>
2020-01-30 19:31:35 +0530

    defined an async task for data fetching

--- app/src/main/java/com/example/alpha/MainActivity.java
+++ app/src/main/java/com/example/alpha/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.alpha;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+data_fetcher("https://cricapi.com/api/cricket/?apikey=Bd8wF5XUVGRFmScoOnpJ5aEh93d2");
+        }
+
+    private void data_fetcher(String url)
+    {
+class data_fetch extends AsyncTask<Void,Void,String>
+{
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
     }
 
+    @Override
+    protected String doInBackground(Void... voids) {
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
+    data_fetch data_fetch=new data_fetch();
+    data_fetch.execute();
+    }
 }
