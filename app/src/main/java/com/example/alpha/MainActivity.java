ea6064f3f413643a026f68ca661c19f01ced849e
harshit <harshitsinghall0006@gmail.com>
2020-01-30 20:50:34 +0530

    score board template added

--- app/src/main/java/com/example/alpha/MainActivity.java
+++ app/src/main/java/com/example/alpha/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.alpha;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ArrayList<String> Number;
+    RecyclerView.LayoutManager RecyclerViewLayoutManager;
+    RecyclerViewAdapter RecyclerViewHorizontalAdapter;
+    LinearLayoutManager HorizontalLayout ;
+    View ChildView ;
+    int RecyclerViewItemPosition ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerview1);
+
+        RecyclerViewLayoutManager = new LinearLayoutManager(getApplicationContext());
+
+        recyclerView.setLayoutManager(RecyclerViewLayoutManager);
+        AddItemsToRecyclerViewArrayList();
+
+        RecyclerViewHorizontalAdapter = new RecyclerViewAdapter(Number);
+
+        HorizontalLayout = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(HorizontalLayout);
+
+        recyclerView.setAdapter(RecyclerViewHorizontalAdapter);
+    }
+    // function to add items in RecyclerView.
+    public void AddItemsToRecyclerViewArrayList(){
+
+        Number = new ArrayList<>();
+        Number.add("ONE");
+        Number.add("TWO");
+        Number.add("THREE");
+        Number.add("FOUR");
+        Number.add("FIVE");
+        Number.add("SIX");
+        Number.add("SEVEN");
+        Number.add("EIGHT");
+        Number.add("NINE");
+        Number.add("TEN");
+
     }
 
 }
