6d7451b955b64f5eaf2ecff90f94f4ab82e42f76
harshit <harshitsinghall0006@gmail.com>
2020-01-31 22:40:14 +0530

    live scores added

--- app/src/main/java/com/example/alpha/Cricket_live_scores.java
+++ app/src/main/java/com/example/alpha/Cricket_live_scores.java
@@ -4,10 +4,12 @@ public class Cricket_live_scores
 {
     String team1;
     String team2;
-    Cricket_live_scores(String team1,String team2)
+    String stat;
+    Cricket_live_scores(String team1,String team2,String stat)
     {
         this.team1=team1;
         this.team2=team2;
+        this.stat=stat;
     }
 
 }
