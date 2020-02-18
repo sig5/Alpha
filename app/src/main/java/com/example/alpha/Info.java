3ab02caf8f786ac1170f3ddf5f171a595ca5d016
Harshit <meharshitkapoor7@gmail.com>
2020-02-18 22:33:16 +0530

    Squads fetched

--- app/src/main/java/com/example/alpha/Info.java
+++ app/src/main/java/com/example/alpha/Info.java
@@ -1,11 +1,45 @@
 package com.example.alpha;
 
+import android.util.JsonReader;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.TreeMap;
+
 public class Info {
-    String toss;
-    String series_name;
-    String match_desc;
-    String[] umpires=new String[2];
-    String venue;
-    String[] squad_team1=new String[15];
-    String[] squad_team2=new String[15];
+    static String[] squad_team1=new String[40];
+    static String[] squad_team2=new String[20];
+    static TreeMap<Integer,String> playerid=new TreeMap<>();
+    public static String[] squads(String json) throws JSONException {
+        JSONObject obj=new JSONObject(json);
+        JSONArray players=obj.getJSONArray("players");
+        int l=players.length();
+        for(int i=0;i<l;i++)
+        {
+            JSONObject p=players.getJSONObject(i);
+            String name=p.getString("f_name");
+            Integer id= Integer.parseInt(p.getString("id"));
+            playerid.put(id,name);
+        }
+        int j=0;
+        JSONObject t1=obj.getJSONObject("team1");
+        squad_team1[j++]=t1.getString("name");
+        JSONArray sq1=t1.getJSONArray("squad");
+        int l1=sq1.length();
+        for(int i=0;i<l1;i++)
+            squad_team1[j++]=playerid.get(sq1.getInt(i));
+        squad_team1[j++]="SPLIT";
+        JSONObject t2=obj.getJSONObject("team2");
+        squad_team1[j++]=t2.getString("name");
+        JSONArray sq2=t2.getJSONArray("squad");
+        int l2=sq2.length();
+        for(int i=0;i<l2;i++)
+            squad_team1[j++]=playerid.get(sq2.getInt(i));
+        for(int i=0;i<j;i++)
+            System.out.print(squad_team1[i]+" ");
+        return squad_team1;
+
+    }
 }
